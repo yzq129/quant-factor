84006d562d0b2753e3eac350ab6b6ee55bdad50a
yzq129 <yuzhenqi1209@163.com>
2026-06-28 21:27:56 +0800

    Update config/docs/strategy comments to 29 factors and current future-function status

--- factor_engine/strategy/pure_ic.py
+++ factor_engine/strategy/pure_ic.py
@@ -1,6 +1,6 @@
 """
 纯 IC 加权策略
-不使用 LightGBM，仅使用 IC 权重打分，用于剥离因子 Alpha
+不使用 LightGBM，仅使用 29 个基础因子的 IC 权重打分，用于剥离因子 Alpha
 """
 from factor_engine.strategy.base import BaseStrategy
 from factor_engine.ic_analysis import FACTOR_NAMES
@@ -13,7 +13,7 @@ class PureICStrategy(BaseStrategy):
         super().__init__(name='Pure_IC')
     
     def get_factor_names(self):
-        """返回 15 个基础因子"""
+        """返回 29 个基础因子"""
         return list(FACTOR_NAMES)
     
     def get_table_suffix(self):
